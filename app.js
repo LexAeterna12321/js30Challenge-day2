eaca71e227fa7c591bf4457f40dfd45bba6552b5
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-29 12:42:25 +0100

    tick fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const hoursArm = document.querySelector(".hour-hand");
 
 const timeFlies = ()=>{
     const now = new Date();
-
+ 
     secsDegree = (now.getSeconds() / 60 * 360)+ 90;
     secsArm.style.transform = `rotate(${secsDegree}deg)`;
 
@@ -13,6 +13,10 @@ const timeFlies = ()=>{
 
     hoursDegree = (now.getHours() / 60 / 12 * 360)+ 90;
     hoursArm.style.transform = `rotate(${hoursDegree}deg)`;
-}
 
+    secsDegree == 90 ? secsArm.style.transition = "0s" : secsArm.style.transition = "all 0.4s cubic-bezier(0.78, 0.63, 0, 1.58) 0s";
+    minsDegree == 90 ? minsArm.style.transition = "0s" : minsArm.style.transition = "all 0.4s cubic-bezier(0.78, 0.63, 0, 1.58) 0s";
+    hoursDegree == 90 ? hoursArm.style.transition = "0s" : hoursArm.style.transition = "all 0.4s cubic-bezier(0.78, 0.63, 0, 1.58) 0s";
+ 
+}
 setInterval(timeFlies,1000);
