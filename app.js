d9188cf61dc36fd452b59afb57e89a692c762792
LexAeterna12321 <lexaeterna12321@gmail.com>
2018-10-29 11:46:25 +0100

    whole code

---
+++ app.js
@@ -0,0 +1,18 @@
+const secsArm = document.querySelector(".second-hand");
+const minsArm = document.querySelector(".min-hand");
+const hoursArm = document.querySelector(".hour-hand");
+
+const timeFlies = ()=>{
+    const now = new Date();
+
+    secsDegree = now.getSeconds() / 60 * 360;
+    secsArm.style.transform = `rotate(${secsDegree}deg)`;
+
+    minsDegree = now.getMinutes() / 60 * 360;
+    minsArm.style.transform = `rotate(${minsDegree}deg)`;
+
+    hoursDegree = now.getHours() / 60 / 12 * 360;
+    hoursArm.style.transform = `rotate(${hoursDegree}deg)`;
+}
+
+setInterval(timeFlies,1000);
